ae2b14dc45fdfdf0b364e06dd1d859d7129d4a46
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-01-22 14:44:42 +0100

    Add files via upload

--- exercise2.php
+++ exercise2.php
@@ -11,35 +11,45 @@
 
   <body>
       <?php
-        $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+      /*
+        for ($number = 1; $number < 11; $number++) {
 
-        for ($i = 0; $i < count($numbers); $i++) {
+          $squaredNumber = pow($number, 2);
+          $cubedNumber = pow($number, 3);
 
-        global $numbers;
+          if (strlen((string)$number) < 2) {
+            echo ($number . "&nbsp;&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
+          } else {
+            echo ($number . "&nbsp;&nbsp; || &nbsp;&nbsp;");
+          }
 
-        $number = $numbers[$i];
-        $squaredNumber = pow($number, 2);
-        $cubedNumber = pow($number, 3);
+          if (strlen((string)$squaredNumber) < 2) {
+            echo ($squaredNumber . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
+          } elseif (strlen((string)$squaredNumber) < 3) {
+            echo ($squaredNumber . "&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
+          } else {
+            echo ($squaredNumber . "&nbsp; || &nbsp;&nbsp;");
+          }
 
-        if (strlen((string)$number) < 2) {
-          echo ($number . "&nbsp;&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
-        } else {
-          echo ($number . "&nbsp;&nbsp; || &nbsp;&nbsp;");
-        }
+          echo $cubedNumber;
 
-        if (strlen((string)$squaredNumber) < 2) {
-          echo ($squaredNumber . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
-        } elseif (strlen((string)$squaredNumber) < 3) {
-          echo ($squaredNumber . "&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
-        } else {
-          echo ($squaredNumber . "&nbsp; || &nbsp;&nbsp;");
-        }
-
-        echo $cubedNumber;
-
-        echo nl2br ("\n");
-        //echo  nl2br ($numbers[$i] . " || " . pow($numbers[$i], 2) . " || " . pow($numbers[$i], 3) . "\n");
+          echo nl2br ("\n");
+          //echo  nl2br ($numbers[$i] . " || " . pow($numbers[$i], 2) . " || " . pow($numbers[$i], 3) . "\n");
       }
+      */
+      echo "<table>";
+      for ($number = 1; $number < 11; $number++) {
+
+          $squaredNumber = pow($number, 2);
+          $cubedNumber = pow($number, 3);
+
+          echo "<tr>";
+          echo "<td>" . $number . "</td>";
+          echo "<td>" . $squaredNumber . "</td>";
+          echo "<td>" . $cubedNumber . "</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
       ?>
   </body>
 </html>
