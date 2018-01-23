4dafba79ca98ae7030fe2c197089199a794e1049
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-01-23 12:24:47 +0100

    Add files via upload

--- exercise2.php
+++ exercise2.php
@@ -11,32 +11,6 @@
 
   <body>
       <?php
-      /*
-        for ($number = 1; $number < 11; $number++) {
-
-          $squaredNumber = pow($number, 2);
-          $cubedNumber = pow($number, 3);
-
-          if (strlen((string)$number) < 2) {
-            echo ($number . "&nbsp;&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
-          } else {
-            echo ($number . "&nbsp;&nbsp; || &nbsp;&nbsp;");
-          }
-
-          if (strlen((string)$squaredNumber) < 2) {
-            echo ($squaredNumber . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
-          } elseif (strlen((string)$squaredNumber) < 3) {
-            echo ($squaredNumber . "&nbsp;&nbsp;&nbsp; || &nbsp;&nbsp;");
-          } else {
-            echo ($squaredNumber . "&nbsp; || &nbsp;&nbsp;");
-          }
-
-          echo $cubedNumber;
-
-          echo nl2br ("\n");
-          //echo  nl2br ($numbers[$i] . " || " . pow($numbers[$i], 2) . " || " . pow($numbers[$i], 3) . "\n");
-      }
-      */
       function makeTable($maxNumber) {
       echo "<table border=1>";
         for ($number = 1; $number <= $maxNumber; $number++) {
